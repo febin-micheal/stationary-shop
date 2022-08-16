d0b6af858f8fd6ae52d93ca5d9828446fc9906c6
Febin <febinboogey@gmail.com>
2022-08-16 11:08:14 +0530

    new file:   app2/forms.py 	modified:   app2/models.py 	new file:   app2/urls.py 	modified:   app2/views.py

--- app2/models.py
+++ app2/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Store(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    name = models.CharField(max_length=30)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
