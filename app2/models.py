1eba6413040cae22cdca1fd86d84ad5b8aee5111
Febin Micheal Antony <43706733+febin-micheal@users.noreply.github.com>
2022-08-18 17:41:29 +0530

    Add files via upload

--- app2/models.py
+++ app2/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Store(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, unique=True)
 
     def __str__(self):
         return self.name
@@ -14,3 +14,11 @@ class Item(models.Model):
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['name', 'store'],
+                name = 'unique_name_store'
+            )
+        ]
