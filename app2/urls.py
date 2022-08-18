f8a1a1453bd1c4e04fea3aada1238682dfe2f7e9
Febin Micheal Antony <43706733+febin-micheal@users.noreply.github.com>
2022-08-19 00:45:38 +0530

    Add files via upload

--- app2/urls.py
+++ app2/urls.py
@@ -4,12 +4,12 @@ from app2 import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    # path('store/', views.store, name='store'),
-    # path('store/<pk>', views.store, name='store'),
-    # path('item/', views.item, name='item'),
-    # path('item/<pk>', views.item, name='item'),
-    path('store/', views.StoreView.as_view(), name='store'),
-    path('store/<pk>', views.StoreView.as_view(), name='store'),
-    path('item/', views.ItemView.as_view(), name='item'),
-    path('item/<pk>', views.ItemView.as_view(), name='item'),
+    path('store/', views.StoreView.as_view(), name='store_list'),
+    path('store/create/', views.StoreView.as_view(), name='store_create'),
+    path('store/update/<pk>', views.StoreView.as_view(), name='store_update'),
+    path('store/delete/<pk>', views.StoreView.as_view(), name='store_delete'),
+    path('item/', views.ItemView.as_view(), name='item_list'),
+    path('item/create/', views.ItemView.as_view(), name='item_create'),
+    path('item/update/<pk>', views.ItemView.as_view(), name='item_update'),
+    path('item/delete/<pk>', views.ItemView.as_view(), name='item_delete'),
 ]
--- app2/views.py
+++ app2/views.py
@@ -1,74 +1,12 @@
 from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
+from django.views import View
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'app2/home.html')
-
-def store(request, pk = None):
-    if pk != None:
-        store = Store.objects.get(id = pk)
-        if request.POST.get('method') == 'delete':
-            store.delete()
-
-        elif request.POST.get('method') == 'edit':
-            form = StoreForm(instance = store)
-
-            return render(request, 'app2/store_update.html', {'form': form, 'store': store})  
-
-        elif request.POST.get('method') == 'update':
-            form = StoreForm(request.POST, instance = store)
-            if form.is_valid():
-                form.save()          
-
-    else:  
-        form = StoreForm(request.POST)
-        if form.is_valid():
-            form.save()
-
-    form = StoreForm
-    store_list = list(Store.objects.all().values())
-    page = {
-        'form': form,
-        'store_list': store_list
-    }
-
-    return render(request, 'app2/store.html', page)
-
-def item(request, pk = None):
-    if pk != None:
-        item = Item.objects.get(id = pk)
-        if request.POST.get('method') == 'delete':
-            item.delete()
-
-        elif request.POST.get('method') == 'edit':
-            form = ItemForm(instance = item)
-
-            return render(request, 'app2/item_update.html', {'form': form, 'item': item})  
-
-        elif request.POST.get('method') == 'update':
-            form = ItemForm(request.POST, instance = item)
-            if form.is_valid():
-                form.save()          
-
-    else:  
-        form = ItemForm(request.POST)
-        if form.is_valid():
-            form.save()
-
-    form = ItemForm
-    item_list = list(Item.objects.all().values('id', 'name', 'store__name'))
-
-    page = {
-        'form': form,
-        'item_list': item_list,
-    }
-
-    return render(request, 'app2/item.html', page)
-
-from django.views import View
+    return render(request, 'home.html')
 
 class ModelView(View):
     
@@ -105,19 +43,18 @@ class ModelView(View):
 
         return redirect(self.success_url)
         
-
 class StoreView(ModelView):
     form_class = StoreForm
-    template_name = 'app2/store.html'
+    template_name = 'store.html'
     model = Store
-    success_url = 'store'
+    success_url = 'store_list'
     fields = ['id', 'name']
-    template_edit = 'app2/store_update.html'
+    template_edit = 'store_update.html'
 
 class ItemView(ModelView):
     form_class = ItemForm
-    template_name = 'app2/item.html'
+    template_name = 'item.html'
     model = Item
-    success_url = 'item'
+    success_url = 'item_list'
     fields = ['id', 'name', 'store__name']
-    template_edit = 'app2/item_update.html'
+    template_edit = 'item_update.html'
