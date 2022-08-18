4367a47bc451c71d7ff4c0ca4afa72aec28b0efc
Febin Micheal Antony <43706733+febin-micheal@users.noreply.github.com>
2022-08-18 23:07:36 +0530

    Add files via upload

--- app2/urls.py
+++ app2/urls.py
@@ -1,10 +1,15 @@
+from typing import ItemsView
 from django.urls import path
 from app2 import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('store/', views.store, name='store'),
-    path('store/<pk>', views.store, name='store'),
-    path('item/', views.item, name='item'),
-    path('item/<pk>', views.item, name='item'),
+    # path('store/', views.store, name='store'),
+    # path('store/<pk>', views.store, name='store'),
+    # path('item/', views.item, name='item'),
+    # path('item/<pk>', views.item, name='item'),
+    path('store/', views.StoreView.as_view(), name='store'),
+    path('store/<pk>', views.StoreView.as_view(), name='store'),
+    path('item/', views.ItemView.as_view(), name='item'),
+    path('item/<pk>', views.ItemView.as_view(), name='item'),
 ]
--- app2/views.py
+++ app2/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from re import template
+from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
-
+from django.http import HttpResponseRedirect, HttpResponse
 # Create your views here.
 
 def home(request):
@@ -67,3 +68,58 @@ def item(request, pk = None):
     }
 
     return render(request, 'app2/item.html', page)
+
+from django.views import View
+
+class ModelView(View):
+    
+    def get(self, request, *args, **kwargs):
+        form = self.form_class
+        object_list = list(self.model.objects.all().values(*self.fields))
+        page = {
+            'form': form,
+            'object_list': object_list
+        }
+        return render(request, self.template_name, page)
+
+    def post(self, request, pk = None, *args, **kwargs):
+        
+        if pk == None:
+            form = self.form_class(request.POST)
+            if form.is_valid():
+                self.model.objects.create(**form.cleaned_data)
+        
+        else:
+            object = self.model.objects.get(id = pk)
+            if request.POST.get('method') == 'delete':
+                object.delete()
+
+            elif request.POST.get('method') == 'edit':
+                form = self.form_class(instance = object)
+
+                return render(request, self.template_edit, {'form': form, 'object': object})  
+
+            elif request.POST.get('method') == 'update':
+                form = self.form_class(request.POST, instance = object)
+                if form.is_valid():
+                    form.save()          
+
+
+        return redirect(self.success_url)
+        
+
+class StoreView(ModelView):
+    form_class = StoreForm
+    template_name = 'app2/store.html'
+    model = Store
+    success_url = 'store'
+    fields = ['id', 'name']
+    template_edit = 'app2/store_update.html'
+
+class ItemView(ModelView):
+    form_class = ItemForm
+    template_name = 'app2/item.html'
+    model = Item
+    success_url = 'item'
+    fields = ['id', 'name', 'store__name']
+    template_edit = 'app2/item_update.html'
